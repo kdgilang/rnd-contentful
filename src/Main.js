0f54aa5c797e4e0430e3e9aa778d8807f92d0083
Gilang Putra <gilang.putra@ogilvy.com.au>
2018-10-17 11:37:02 +0800

    maintenance mode on

--- src/Main.js
+++ src/Main.js
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Blog from './pages/Blog';
 import Single from './pages/Single';
 import Login from './pages/Login';
+import Maintenance from './pages/Maintenance';
 export default () => {
 	return (
 	  <main>
@@ -11,6 +12,7 @@ export default () => {
 	      <Route exact path='/' component={Home}/>
 	      <Route path='/blog' component={Blog} />
 	      <Route path='/single/:slug' component={Single}/>
+	      <Route path='/maintenance' component={Maintenance}/>
 	      <Route path='/login' component={Login}/>
 	    </Switch>
 	  </main>
---
+++ src/pages/Maintenance.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+	constructor(props) {
+		super(props)
+	}
+	render() {
+		return (
+			<div id="blog">
+				<div className="fix-header">
+					<div className="container">
+						<div className="row">
+							<div className="col-12 text-center">
+								<div class="panel p-5">
+									<h1>Still Maintenance Bro!!</h1>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		)
+	}
+}
+
+export default Home;
